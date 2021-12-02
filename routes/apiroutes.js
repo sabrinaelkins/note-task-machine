1829f67d875b12fdb2f9890937a1a8b92c621073
sabrinaelkins <sabrina.elkins12@gmail.com>
2021-12-02 15:07:51 -0600

    server.js routes to html file and api file

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -6,7 +6,16 @@ router.get("/api/notes", function(req,res){
     db = JSON.stringify(fs.readFileSync)("./db/db.json")
     res.json(db)
 })
-
+router.post("/api/notes", function(req,res){
+    var apiNotes = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(math.random()*1000)
+    }
+    db.push(apiNotes)
+    fs.writeFileSync("./db/db.json", JSON.stringify(db))
+    res.json(db)
+})
 
 
 
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3003;
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static("public"))
-
+app.use(require("./routes/apiroutes"))
+app.use(require("./routes/htmlroutes"))
 
 
 app.listen(PORT, function(){
