14ad05212acb80321be0ba2545911791b15fa36b
sabrinaelkins <sabrina.elkins12@gmail.com>
2022-02-09 13:42:31 -0600

    funtioning note

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,22 +1,33 @@
 const router = require("express").Router()
-const db = require("../db/db.json")
-
+let db = require("../db/db.json")
+const fs = require("fs")
 
 router.get("/api/notes", function(req,res){
-    db = JSON.stringify(fs.readFileSync)("./db/db.json")
+    db = JSON.parse(fs.readFileSync("./db/db.json","utf-8")) || []
+    console.log("GET",db)
     res.json(db)
 })
 router.post("/api/notes", function(req,res){
     var apiNotes = {
         title: req.body.title,
         text: req.body.text,
-        id: Math.floor(math.random()*1000)
+        id: Math.floor(Math.random()*1000)
     }
     db.push(apiNotes)
     fs.writeFileSync("./db/db.json", JSON.stringify(db))
+    console.log("Post",db)
     res.json(db)
 })
 
+router.delete("/api/notes/id",function(req,res){
+    let recentNoteList = db.filter(note => note.id != req.params.id)
+    db= recentNoteList;
+    fs.writeFileSync("./db/db.json", JSON.stringify(db))
+    console.log("del",db)
+    res.json(db)
+})
+
+module.exports = router;
 
 
 
@@ -28,4 +39,3 @@ router.post("/api/notes", function(req,res){
 
 
 
-module.exports = router
